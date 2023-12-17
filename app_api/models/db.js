a4f90e7b35751af968aa67a4795b54aaca78db34
Yağmur Yalçın <yagmuryalcin26@hotmail.com>
2023-12-17 15:55:17 +0300

    odevson

---
+++ app_api/models/db.js
@@ -0,0 +1,19 @@
+var mongoose = require("mongoose");
+var dbURI =
+  "mongodb+srv://mekan32:<password>@mekanbul.a2excj8.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(dbURI);
+mongoose.connection.on("connected", function () {
+  console.log(dbURI + "bağlandı");
+});
+mongoose.connection.on("error", function () {
+  console.log("bağlantı hatası");
+});
+mongoose.connection.on("disconnected", function () {
+  console.log("bağlantı kesildi");
+});
+process.on("SIGINT", function () {
+  mongoose.connection.close();
+  console.log("kapatıldı");
+  process.exit(0);
+});
+require("./venue");
